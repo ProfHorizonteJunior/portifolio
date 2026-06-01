1ff0f58ecc056fab4287f0dd15b1470ef84b7151
Marcelo Augusto <marceloaugustosouzapassos@gmail.com>
2026-06-01 19:43:23 -0300

    feat: cria landing page do joao na rota /marcelo

--- portifolio-landing-page/src/pages/MarceloAugusto.js
+++ portifolio-landing-page/src/pages/MarceloAugusto.js
@@ -1,7 +1,7 @@
 export default function Joao() {
   return (
     <div>
-      <h1>Olá, eu sou o marcelo</h1>
+      <h1>Olá, eu sou o marcelo</h1>git push origin feature/landing-page-joao
       <p>Bem-vindo à minha landing page!</p>
     </div>
   );
